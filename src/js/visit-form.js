60f95c3ff5c9c504bb27834820b507413600702b
fortavey <fortavey@gmail.com>
2021-01-29 15:37:49 +0200

    end main page

--- src/js/visit-form.js
+++ src/js/visit-form.js
@@ -1,5 +1,67 @@
 const visitFormVue = new Vue({
   el: '.visit-form',
+  template: `
+  <div class="visit-form">
+          <div class="visit-form__title">
+              Вызвать мастера <strong>БЕСПЛАТНО</strong> <br>
+              по тел: +7 (495) 000-00-00 <br>
+              или <br>
+              <strong>заполните форму</strong>
+            </div>
+            <div class="visit-form__item yes-select" @click="changeTypeShow(true)">
+              <span class="sp">Выберите вид ремонта:</span>
+              <div class="visit-form__repair-type">{{ repairType }}</div>
+            </div>
+            <div class="visit-form__item yes-select" @click="changeDistrictShow(true)">
+              <span class="sp">В каком районе Москвы Вы находитесь:</span>
+              <div class="visit-form__district">{{ district }}</div>
+            </div>
+            <div class="visit-form__item" @click="changeCalendarShow(true)">
+              <span class="sp">Когда к Вам приехать</span>
+              <div class="visit-form__date">{{ renderDate }}
+                <div class="calendar-picker" :style="calendarStyle" @click.stop>
+                  <v-date-picker v-model="date" @dayclick="changeCalendarShow(false)" />
+                </div>
+              </div>
+            </div>
+            <div class="visit-form__item">
+              <div class="visit-form__phone">
+                <input type="tel" placeholder="Ваш телефон" v-model="phone">
+              </div>
+            </div>
+            <div class="visit-form__submit">Вызвать мастера</div>
+
+            <div class="choose-district choose-value" 
+                  :style="districtStyle"
+                  @click="changeDistrictShow(false)">
+              <div class="choose-value__close"><img src="./assets/img/close.svg" alt="close"></div>
+              <div class="choose-value__title">Выберите район</div>
+              <div class="choose-value__container">
+                <div class="choose-value__item"
+                      v-for="(value, index) in districts"
+                      @click="setDistrict(index)"
+                >
+                {{ value }}
+                </div>
+              </div>
+            </div>
+
+            <div class="choose-type choose-value" 
+                  :style="typeStyle"
+                  @click="changeTypeShow(false)">
+              <div class="choose-value__close"><img src="./assets/img/close.svg" alt="close"></div>
+              <div class="choose-value__title">Выберите вид ремонта</div>
+              <div class="choose-value__container">
+                <div class="choose-value__item"
+                      v-for="(value, index) in repairTypes"
+                      @click="setType(index)"
+                >
+                {{ value }}
+                </div>
+              </div>
+            </div>
+  </div>
+  `,
   data: {
     repairTypes: [],
     repairType: '',
--- webpack.config.js
+++ webpack.config.js
@@ -74,7 +74,6 @@ module.exports = {
         new webpack.SourceMapDevToolPlugin({
             filename: '[file].map'
         }),
-        new webpack.HotModuleReplacementPlugin()
     ],
     module: {
         rules: [
